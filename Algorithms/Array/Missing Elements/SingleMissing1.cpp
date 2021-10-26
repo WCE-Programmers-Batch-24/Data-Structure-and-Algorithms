c7b6fd667cbbf20e34da440b83e7a4e0c0860c54
RAOexe <2020bteit00034@walchandsangli.ac.in>
2021-10-26 21:59:04 +0530

    Add: Function to find single missing element in sorted array

---
+++ Algorithms/Array/Missing Elements/SingleMissing1.cpp
@@ -0,0 +1,50 @@
+/*
+  __  __   _               _                  
+ |  \/  | (_)  ___   ___  (_)  _ __     __ _  
+ | |\/| | | | / __| / __| | | | '_ \   / _` | 
+ | |  | | | | \__ \ \__ \ | | | | | | | (_| | 
+ |_|  |_| |_| |___/ |___/ |_| |_| |_|  \__, | 
+                                       |___/  
+*/
+
+/*
+  Using sum of n terms. 👇
+      Displays the sinlge missing element in the *sorted array*.
+*/
+void singleMissing(int A[], int size){
+    int first = A[0],
+        last = A[size-1];
+    
+    int sum = 0, s1 = 0, s2 = 0, c = 0;
+    
+    // Storing sum of array elements in 'sum'
+    for(int i=0; i<size; i++){
+        sum += A[i];
+    }
+    
+    // Checking is (A[0] - 1) is Odd or Even 
+    if((first-1) & 1){
+
+        // if Odd
+        s1 += (first-1)*(((first-1) + 1)/2);
+
+    } // if Even
+    else s1 += ( (first-1)/2 )*((first-1) + 1);
+    
+    // Checking is A[size-1] is Odd or Even 
+    if((last & 1)){
+
+        //if Odd
+        s2 += last*((last + 1)/2);
+
+    } // if Even
+    else s2 += (last/2)*(last + 1);
+
+    c += s2 - s1; // The expected sum
+
+
+    /*
+      Answer = Expected Sum - Array Sum
+    */    
+    cout<<c - sum<<"\n";
+}
