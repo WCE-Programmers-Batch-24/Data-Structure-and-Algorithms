2526d9d84adaf2a241f285305ffa41df16a7a906
RAOexe <2020bteit00034@walchandsangli.ac.in>
2021-10-26 22:32:45 +0530

    Add: Function to print multiple missing elements in sorted array.

---
+++ Algorithms/Array/Missing Elements/MultipleMissing1.cpp
@@ -0,0 +1,25 @@
+/*
+  Method 2: Using Index Subtraction. 👇
+            Displays the missing element in the *sorted array*.
+*/
+
+void multipleMissing(int A[], int size){
+    
+    // Difference between 0th element and it's index taken as base difference
+    int diff = A[0] - 0;
+
+    for(int i=0; i<size; i++){
+
+        // If current difference(A[i] - i) not same as base difference then display the element.
+        if( (A[i] - i)!= diff ){
+
+            // Printing the element and increasing the base difference till it equals to current difference.
+            while (diff < (A[i]-i)){
+                cout<<i+diff<<" ";
+                diff++;
+            }
+        }
+    }
+
+    cout<<"\n";
+}
